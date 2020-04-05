5a53d37425b339ed93c9c0685a5c2c0b8ae96582
Ernest Fakudze <ernestfakudze@rhs.org.uk>
2020-04-05 21:31:39 +0100

    Work in progress

---
+++ MediaRentals.Infrastructure.EntityFramework/Configuration/BookAuthorConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MediaRentals.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MediaRentals.Infrastructure.EntityFramework.Configuration
+{
+    public class BookAuthorConfiguration: IEntityTypeConfiguration<BookAuthor>
+    {
+        public BookAuthorConfiguration()
+        {
+        }
+
+        public void Configure(EntityTypeBuilder<BookAuthor> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MediaRentals.Infrastructure.EntityFramework/MediaRentalsContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediaRentals.Infrastructure.EntityFramework
+{
+    public class MediaRentalsContext : DbContext
+    {
+        public MediaRentalsContext()
+        {
+        }
+
+        public MediaRentalsContext(DbContextOptions<MediaRentalsContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
